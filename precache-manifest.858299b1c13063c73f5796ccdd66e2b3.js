5655d7f4713c0cb1fab7b09d1ed3a992a43d28e5
Kristedd <kristiangerkman1@gmail.com>
2021-12-08 09:09:31 +0200

    build

---
+++ precache-manifest.858299b1c13063c73f5796ccdd66e2b3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "32b7492fc866e86e520b",
+    "url": "/react-pixel-art-maker/static/css/main.fbaf0083.chunk.css"
+  },
+  {
+    "revision": "32b7492fc866e86e520b",
+    "url": "/react-pixel-art-maker/static/js/main.0afcee77.chunk.js"
+  },
+  {
+    "revision": "70563fc66424b82d8c85",
+    "url": "/react-pixel-art-maker/static/js/runtime~main.7d051342.js"
+  },
+  {
+    "revision": "7eae23de4419388746db",
+    "url": "/react-pixel-art-maker/static/js/2.c6358ac1.chunk.js"
+  },
+  {
+    "revision": "6c13cf5fee4ea24e4ee454b2b39d8641",
+    "url": "/react-pixel-art-maker/index.html"
+  }
+];
